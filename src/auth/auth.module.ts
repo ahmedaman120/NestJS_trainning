daaed7f5d1a76fd6dc34becaf565c741f55e7434
ahmedaman120 <ahmedayman.aa191@gmail.com>
2022-04-13 01:05:01 +0200

    add proxy service to make it to comunicate with database using prisma client

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+// import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthControler } from './auth.controler';
 import { AuthService } from './auth.service';
 
---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,23 @@
+import { Module, Global } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from './prisma.service';
+import { PrismaController } from './prisma.controller';
+
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+  controllers: [PrismaController]
+})
+export class PrismaModule extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:postgres@localhost:5431/nest?schema=public',
+        },
+      },
+    });
+  }
+}
